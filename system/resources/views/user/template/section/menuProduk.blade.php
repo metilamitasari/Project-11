2c18b6527e16cc41f5bc427194df024d20d20c12
metilamitasari <71206945+metilamitasari@users.noreply.github.com>
2021-01-16 19:54:31 +0700

    Initial commit

---
+++ system/resources/views/user/template/section/menuProduk.blade.php
@@ -0,0 +1,11 @@
+    		<div class="row justify-content-center">
+    			<div class="col-md-10 mb-5 text-center">
+    				<ul class="product-category">
+    					<li><a href="#" class="active">All</a></li>
+    					<li><a href="#">Aksesoris</a></li>
+    					<li><a href="#">mandau</a></li>
+    					<li><a href="#">gelang</a></li>
+    					<li><a href="#">cincin</a></li>
+    				</ul>
+    			</div>
+    		</div>
